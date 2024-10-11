9afda80ebe070ffc1d499de7c21ca504f02d1abd
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-11 03:16:49 +0000

    involved type annotations

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""More involved type annotations"""
+
+
+def safely_get_value(dct: Mapping, key: Any, default=None):
+    """involved type annotations"""
+    if key in dct:
+        return dct[key]
+    else:
+        return default
