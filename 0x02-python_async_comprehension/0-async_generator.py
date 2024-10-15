df0c8982795a0ca15fb31b4d86bd8ac0898db237
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-15 22:13:49 +0000

    a coroutine that takes no arguments.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Async generator"""
+
+
+from typing import AsyncGenerator
+import random
+import asyncio
+
+
+async def async_generator() -> AsyncGenerator[int, None]:
+    """a coroutine that takes no arguments."""
+    for n in range(10):
+        rand = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield rand
