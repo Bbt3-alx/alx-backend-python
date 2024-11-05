d23fc0c28a119befd95504a3e51858fd4357f041
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-05 01:19:30 +0000

    Mock get_json to check response handling.

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import Mock, patch, MagicMock
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +31,24 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Mock HTTP calls"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("utils.requests")
+    def test_get_json(self, test_url, test_payload, mock_request):
+        """Mock get_json to check response handling."""
+        mock_response = MagicMock(status_code=200)
+        mock_response.json.return_value = test_payload
+        mock_request.get.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
 
 
 if __name__ == "__main__":
