911121a23c22c7d5f4685a762c1ba56d8a885700
Brehyma Traore <brehymatraore50@gmail.com>
2024-11-05 02:00:13 +0000

    Tes memoize decorator

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import Mock, patch, MagicMock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,6 +49,40 @@ class TestGetJson(unittest.TestCase):
         mock_response.json.return_value = test_payload
         mock_request.get.return_value = mock_response
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Memoization test"""
+
+    def test_memoize(self):
+        """Tes memoize decorator"""
+
+        class TestClass:
+            """Class to Test memoize decorator"""
+
+            def a_method(self):
+                """only called once using assert_called_once."""
+                return 42
+
+            @memoize
+            def a_property(self):
+                """
+                When calling twice, the correct result is returned
+                """
+                return self.a_method()
+
+        test_instance = TestClass()
+        test_instance.a_method = MagicMock(return_value=42)
+
+        # First access: calls `a_method` and caches the result
+        result_first_call = test_instance.a_property
+        test_instance.a_method.assert_called_once()
+        self.assertEqual(result_first_call, 42)
+
+        # Second access: should return cached result, not `a_method`
+        result_second_call = test_instance.a_property
+        test_instance.a_method.assert_called_once()
+        self.assertEqual(result_second_call, 42)
 
 
 if __name__ == "__main__":
